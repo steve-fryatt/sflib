5773888483427a2b055c12df885918e525453e37
Steve Fryatt <info@stevefryatt.org.uk>
2011-06-13 22:28:14 +0000

    Refactor Resources module.

--- src/resources.c
+++ src/resources.c
@@ -1,6 +1,12 @@
-/* SF-Lib - Resources.c
+/**
+ * \file: resources.c
  *
- * Version 0.10 (5 May 2003)
+ * SF-Lib - Resources.c
+ *
+ * (C) Stephen Fryatt, 2003-2011
+ *
+ * Generic RISC OS resource handling for internationalisation and other
+ * shared data.
  */
 
 /* OS-Lib header files. */
@@ -21,65 +27,69 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* ================================================================================================================== */
 
- void find_resource_path (char *path, int size)
+/* Locate a regional resource directory on the specified path, updating the
+ * path in the process.
+ *
+ * This is an external interface, documented in resources.h
+ */
+
+void resources_find_path(char *path, size_t size)
 {
-  int len;
-  char *new_path;
+	int	len;
+	char	*new_path;
 
-  new_path = (char *) malloc (size);
+	new_path = malloc(size);
 
-  if (new_path != NULL)
-  {
-    strcat (path, ".");
-    strcpy (new_path, path);
+	if (new_path == NULL)
+		return;
 
-    len = strlen (new_path);
-    territory_number_to_name (territory_number (), new_path + len, size - len);
+	strcat(path, ".");
+	strcpy(new_path, path);
 
-    if (osfile_read_stamped_no_path (new_path, NULL, NULL, NULL, NULL, NULL) == fileswitch_IS_DIR)
-      strcpy (path, new_path);
-    else
-      if ((size - len) >= 2)
-        strcat (path, "UK");
-  }
+	len = strlen(new_path);
+	territory_number_to_name(territory_number(), new_path + len, size - len);
+
+	if (osfile_read_stamped_no_path (new_path, NULL, NULL, NULL, NULL, NULL) == fileswitch_IS_DIR)
+		strcpy(path, new_path);
+	else if ((size - len) >= 2)
+		strcat(path, "UK");
 }
 
-/* ================================================================================================================== */
 
-osspriteop_area *load_user_sprite_area (char *file)
+/* Load a spritefile into a user sprite area, claiming the necessary memory
+ * and returning its pointer.
+ *
+ * This is an external interface, documented in resources.h
+ */
+
+osspriteop_area *resources_load_user_sprite_area(char *file)
 {
-  int                    size, type;
-  fileswitch_object_type object;
-  osspriteop_area        *area;
-  char                   *suffix, fullfile[1024];
+	int			size, type;
+	fileswitch_object_type	object;
+	osspriteop_area		*area;
+	char			*suffix, fullfile[1024];
 
+	suffix = wimpreadsysinfo_sprite_suffix();
+	snprintf(fullfile, sizeof(fullfile), "%s%s", file, suffix);
 
-  suffix = wimpreadsysinfo_sprite_suffix ();
-  snprintf (fullfile, sizeof(fullfile), "%s%s", file, suffix);
+	object = osfile_read_stamped_no_path(fullfile, NULL, NULL, &size, NULL, (bits *) &type);
 
-  object = osfile_read_stamped_no_path (fullfile, NULL, NULL, &size, NULL, (bits *) &type);
+	if (object != fileswitch_IS_FILE || type != 0xff9) {
+		strcpy(fullfile, file);
+		object = osfile_read_stamped_no_path(fullfile, NULL, NULL, &size, NULL, (bits *) &type);
+	}
 
-  if (!(object == fileswitch_IS_FILE && type == 0xff9))
-  {
-    strcpy (fullfile, file);
-    object = osfile_read_stamped_no_path (fullfile, NULL, NULL, &size, NULL, (bits *) &type);
-  }
+	if (object != fileswitch_IS_FILE || type != 0xff9)
+		return NULL;
 
-  if (object == fileswitch_IS_FILE && type == 0xff9)
-  {
-    size += 4;
-    area = (osspriteop_area *) malloc (size);
-    area->size = size;
-    area->first = 16;
+	size += 4;
+	area = malloc(size);
+	area->size = size;
+	area->first = 16;
 
-    osspriteop_load_sprite_file (osspriteop_NAME, area, fullfile);
-  }
-  else
-  {
-    area = NULL;
-  }
+	osspriteop_load_sprite_file(osspriteop_NAME, area, fullfile);
 
-  return (area);
+	return area;
 }
+
--- src/resources.h
+++ src/resources.h
@@ -1,15 +1,42 @@
-/* SF-Lib - Resources.h
+/**
+ * \file: resources.h
  *
- * Version 0.10 (5 May 2003)
+ * SF-Lib - Resources.h
+ *
+ * (C) Stephen Fryatt, 2003-2011
+ *
+ * Generic RISC OS resource handling for internationalisation and other
+ * shared data.
  */
 
-#ifndef _SFLIB_RESOURCES
-#define _SFLIB_RESOURCES
+#ifndef SFLIB_RESOURCES
+#define SFLIB_RESOURCES
 
-/* ================================================================================================================== */
+#include <stdlib.h>
+#include "oslib/osspriteop.h"
 
-void find_resource_path (char *path, int size);
 
-osspriteop_area *load_user_sprite_area (char *file);
+/**
+ * Locate a regional resource directory on the specified path, updating the
+ * path in the process.
+ *
+ * \param *path		Buffer containing base resource path and space for
+ *			regional folder name.
+ * \param size		The size of the buffer.
+ */
+
+void resources_find_path(char *path, size_t size);
+
+
+/**
+ * Load a spritefile into a user sprite area, claiming the necessary memory
+ * and returning its pointer.
+ *
+ * \param *file		The sprite file to load.
+ * \return		Pointer to the sprite area, or NULL on failure.
+ */
+
+osspriteop_area *resources_load_user_sprite_area(char *file);
 
 #endif
+
