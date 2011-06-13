e36b746804de2a6aee5f445b8efb7e4dec3f3ed4
Steve Fryatt <info@stevefryatt.org.uk>
2011-06-13 21:54:25 +0000

    Refactor Msgs module.

--- src/msgs.c
+++ src/msgs.c
@@ -1,6 +1,12 @@
-/* SF-Lib - Msgs.c
+/**
+ * \file: msgs.c
  *
- * Version 0.10 (22 September 2003)
+ * SF-Lib - Msgs.c
+ *
+ * (C) Stephen Fryatt, 2003-2011
+ *
+ * RISC OS Message File support.  Open and close Message files, and look
+ * up tokens.
  */
 
 /* Acorn C header files. */
@@ -21,132 +27,138 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* ================================================================================================================== */
 
-static messagetrans_control_block *message_block = NULL;
-static int                        external_file = FALSE;
+static messagetrans_control_block	*message_block = NULL;
+static osbool				external_file = FALSE;
 
-/* ================================================================================================================== */
 
-int msgs_init (char *messages_file)
+/* Iniitialise the Msgs module, loading the specified file and preparing the
+ * system to handle message lookups.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+osbool msgs_initialise(char *messages_file)
 {
-  int  message_size;
-  char *message_buffer;
+	int	message_size;
+	char	*message_buffer;
 
-  messagetrans_file_info (messages_file, NULL, &message_size);
-  message_block = (messagetrans_control_block *) malloc (sizeof (messagetrans_control_block));
-  message_buffer = (char *) malloc (message_size);
-  messagetrans_open_file (message_block, messages_file, message_buffer);
+	messagetrans_file_info (messages_file, NULL, &message_size);
+	message_block = (messagetrans_control_block *) malloc (sizeof (messagetrans_control_block));
+	message_buffer = (char *) malloc (message_size);
+	messagetrans_open_file (message_block, messages_file, message_buffer);
 
-  return 1;
+	return TRUE;
 }
 
-/* ------------------------------------------------------------------------------------------------------------------ */
 
-int msgs_external_init (messagetrans_control_block *block)
+/* Initialise the Msgs module using an already prepared MessageTrans
+ * Control Block.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+osbool msgs_initialise_external(messagetrans_control_block *block)
 {
-  message_block = block;
-  external_file = TRUE;
+	message_block = block;
+	external_file = TRUE;
 
-  return 1;
+	return TRUE;
 }
 
-/* ------------------------------------------------------------------------------------------------------------------ */
 
-int msgs_close_file (void)
+/* Terminate the Msgs module, closing any Messages file that was opened
+ * via msgs_initialise().
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+osbool msgs_terminate(void)
 {
-  if (external_file == FALSE)
-  {
-    messagetrans_close_file (message_block);
-    free (message_block);
-  }
+	if (message_block == NULL)
+		return FALSE;
 
-  message_block = NULL;
-  external_file = FALSE;
+	if (external_file == FALSE) {
+		messagetrans_close_file(message_block);
+		free(message_block);
+	}
 
-  return 1;
+	message_block = NULL;
+	external_file = FALSE;
+
+	return TRUE;
 }
 
-/* ================================================================================================================== */
 
-char *msgs_lookup (char *token, char *buffer, int buffer_size)
+/* Look up a message token without parameters, storing the result in the
+ * supplied buffer.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+char *msgs_lookup(char *token, char *buffer, size_t buffer_size)
 {
-  return (msgs_param_lookup (token, buffer, buffer_size, NULL, NULL, NULL, NULL));
+	msgs_param_lookup_result(token, buffer, buffer_size, NULL, NULL, NULL, NULL);
+
+	return buffer;
 }
 
-/* ------------------------------------------------------------------------------------------------------------------ */
 
-char *msgs_param_lookup (char *token, char *buffer, int buffer_size, char *a, char *b, char *c, char *d)
+/* Look up a message token, substituting the supplied parameters and storing
+ * the result in the supplied buffer.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+char *msgs_param_lookup(char *token, char *buffer, size_t buffer_size, char *a, char *b, char *c, char *d)
 {
-  os_error *error;
-  char     *text;
+	msgs_param_lookup_result(token, buffer, buffer_size, a, b, c, d);
 
-
-  text = strchr (token, ':');
-  if (text != NULL)
-  {
-    *text++ = '\0';
-  }
-  else
-  {
-    text = token;
-  }
-
-  error = xmessagetrans_lookup (message_block, token, buffer, buffer_size, a, b, c, d, NULL, NULL);
-  if (error != NULL)
-  {
-    if (strlen (text) < buffer_size)
-    {
-      strcpy (buffer, text);
-    }
-    else
-    {
-      strncpy (buffer, text, buffer_size - 1);
-      *(buffer + buffer_size - 1) = '\0';
-    }
-  }
-
-  return (buffer);
+	return buffer;
 }
 
-/* ================================================================================================================== */
 
-int msgs_lookup_result (char *token, char *buffer, int buffer_size)
+/* Look up a message token without parameters, storing the result in the
+ * supplied buffer.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+osbool msgs_lookup_result(char *token, char *buffer, size_t buffer_size)
 {
-  return (msgs_param_lookup_result (token, buffer, buffer_size, NULL, NULL, NULL, NULL));
+	return msgs_param_lookup_result(token, buffer, buffer_size, NULL, NULL, NULL, NULL);
 }
 
-/* ------------------------------------------------------------------------------------------------------------------ */
 
-int msgs_param_lookup_result (char *token, char *buffer, int buffer_size, char *a, char *b, char *c, char *d)
+/* Look up a message token, substituting the supplied parameters and storing
+ * the result in the supplied buffer.
+ *
+ * This is an external interface, documented in msgs.h
+ */
+
+osbool msgs_param_lookup_result(char *token, char *buffer, size_t buffer_size, char *a, char *b, char *c, char *d)
 {
-  os_error *error;
-  char     *text;
+	os_error	*error;
+	char		*text;
 
+	text = strchr(token, ':');
+	if (text != NULL)
+		*text++ = '\0';
+	else
+		text = token;
 
-  text = strchr (token, ':');
-  if (text != NULL)
-  {
-    *text++ = '\0';
-  }
-  else
-  {
-    text = token;
-  }
+	error = xmessagetrans_lookup(message_block, token, buffer, buffer_size, a, b, c, d, NULL, NULL);
 
-  error = xmessagetrans_lookup (message_block, token, buffer, buffer_size, a, b, c, d, NULL, NULL);
-  if (error != NULL)
-  {
-    if (strlen (text) < buffer_size)
-    {
-      strcpy (buffer, text);
-    }
-    else
-    {
-      strncpy (buffer, text, buffer_size - 1);
-      *(buffer + buffer_size - 1) = '\0';
-    }
-  }
+	if (error == NULL)
+		return FALSE;
 
-  return (error != 0);
+	if (strlen(text) < buffer_size) {
+		strcpy(buffer, text);
+	} else {
+		strncpy(buffer, text, buffer_size - 1);
+		*(buffer + buffer_size - 1) = '\0';
+	}
+
+	return TRUE;
 }
+
--- src/msgs.h
+++ src/msgs.h
@@ -1,25 +1,111 @@
-/* SF-Lib - Msgs.h
+/**
+ * \file: msgs.h
  *
- * Version 0.10 (22 September 2003)
+ * SF-Lib - Msgs.h
+ *
+ * (C) Stephen Fryatt, 2003-2011
+ *
+ * RISC OS Message File support.  Open and close Message files, and look
+ * up tokens.
  */
 
-#ifndef _SFLIB_MSGS
-#define _SFLIB_MSGS
+#ifndef SFLIB_MSGS
+#define SFLIB_MSGS
 
-#ifndef messagetrans_H
+#include <stdlib.h>
 #include "oslib/messagetrans.h"
+
+
+/**
+ * Iniitialise the Msgs module, loading the specified file and preparing the
+ * system to handle message lookups.
+ *
+ * \param *messages_file	The file to open.
+ * \return			TRUE if successful; else FALSE.
+ */
+
+osbool msgs_initialise(char *messages_file);
+
+
+/**
+ * Initialise the Msgs module using an already prepared MessageTrans
+ * Control Block.
+ *
+ * \param *block		The MessageTrans Control Block to use.
+ * \return			TRUE if successful; else FALSE.
+ */
+
+osbool msgs_initialise_external(messagetrans_control_block *block);
+
+
+/**
+ * Terminate the Msgs module, closing any Messages file that was opened
+ * via msgs_initialise().
+ *
+ * \return			TRUE if successful; else FALSE.
+ */
+
+osbool msgs_terminate(void);
+
+
+/**
+ * Look up a message token without parameters, storing the result in the
+ * supplied buffer.
+ *
+ * \param *token		The message token to look up.
+ * \param *buffer		The buffer to hold the result.
+ * \param buffer_size		The size of the result buffer.
+ * \return			A pointer to the result.
+ */
+
+char *msgs_lookup(char *token, char *buffer, size_t buffer_size);
+
+
+/**
+ * Look up a message token, substituting the supplied parameters and storing
+ * the result in the supplied buffer.
+ *
+ * \param *token		The message token to look up.
+ * \param *buffer		The buffer to hold the result.
+ * \param buffer_size		The size of the result buffer.
+ * \param *a			Parameter for %0.
+ * \param *b			Parameter for %1.
+ * \param *c			Parameter for %2.
+ * \param *d			Parameter for %3.
+ * \return			A pointer to the result.
+ */
+
+char *msgs_param_lookup(char *token, char *buffer, size_t buffer_size, char *a, char *b, char *c, char *d);
+
+
+/**
+ * Look up a message token without parameters, storing the result in the
+ * supplied buffer.
+ *
+ * \param *token		The message token to look up.
+ * \param *buffer		The buffer to hold the result.
+ * \param buffer_size		The size of the result buffer.
+ * \return			TRUE if the token was found; else FALSE.
+ */
+
+osbool msgs_lookup_result(char *token, char *buffer, size_t buffer_size);
+
+
+/**
+ * Look up a message token, substituting the supplied parameters and storing
+ * the result in the supplied buffer.
+ *
+ * \param *token		The message token to look up.
+ * \param *buffer		The buffer to hold the result.
+ * \param buffer_size		The size of the result buffer.
+ * \param *a			Parameter for %0.
+ * \param *b			Parameter for %1.
+ * \param *c			Parameter for %2.
+ * \param *d			Parameter for %3.
+ * \return			TRUE if the token was found; else FALSE.
+ */
+
+osbool msgs_param_lookup_result(char *token, char *buffer, size_t buffer_size, char *a, char *b, char *c, char *d);
+
 #endif
 
-/* ================================================================================================================== */
-
-int msgs_init (char *messages_file);
-int msgs_external_init (messagetrans_control_block *block);
-int msgs_close_file (void);
-
-char *msgs_lookup (char *token, char *buffer, int buffer_size);
-char *msgs_param_lookup (char *token, char *buffer, int buffer_size, char *a, char *b, char *c, char *d);
-
-int msgs_lookup_result (char *token, char *buffer, int buffer_size);
-int msgs_param_lookup_result (char *token, char *buffer, int buffer_size, char *a, char *b, char *c, char *d);
-
-#endif
