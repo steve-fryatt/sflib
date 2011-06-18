8096fcb5637782788b34bf9e7e424cc4320afeed
Steve Fryatt <info@stevefryatt.org.uk>
2011-06-18 22:24:23 +0000

    Refactor Transfer module.

--- src/transfer.h
+++ src/transfer.h
@@ -1,10 +1,15 @@
-/* SF-Lib - Transfer.h
+/**
+ * \file: transfer.h
  *
- * Version 0.20 (20 December 2003)
+ * SF-Lib - Transfer.h
+ *
+ * (C) Stephen Fryatt, 2003-2011
+ *
+ * Structured support for the RISC OS Data Transfer Protocol.
  */
 
-#ifndef _SFLIB_TRANSFER
-#define _SFLIB_TRANSFER
+#ifndef SFLIB_TRANSFER
+#define SFLIB_TRANSFER
 
 /* ================================================================================================================== */
 
@@ -13,124 +18,141 @@
  * NoRAMforXFer: Shown if a data-load process fails due to lack of RAM.
  */
 
-/* ==================================================================================================================
- * Saving data
+
+/**
+ * Initialise a Data Save process.
+ *
+ * Copy w, i, pos and ref from a Message_DataRequest for clipboard transfers.
+ *
+ * \param w		The destination window handle.
+ * \param i		The destination icon handle.
+ * \param pos		The destination pointer position. \TODO -- Not a pointer ?!?
+ * \param ref		MyRef from Message_DataRequest, or zero.
+ * \param **data	Pointer to data pointer, to allow for flex blocks.
+ * \param length	Length of data to be transferred.
+ * \param type		Filetype of data.
+ * \param *name		Pointer to proposed leafname for data.
+ * \return		0 if the transfer is started OK; -1 for an error.
  */
 
-int send_start_data_save_block (wimp_w w, wimp_i i, os_coord pos, int ref, char **data,
-                                int length, int type, char *filename);
+int transfer_save_start_block(wimp_w w, wimp_i i, os_coord pos, int ref, char **data,
+		int length, int type, char *name);
 
-/* Start a datasave process.
+
+/**
+ * Start a datasave process using a function to save the data once the pathname
+ * has been found.
  *
- * w, i, pos - Window, icon and pointer position of destination: copy from Message_DataRequest for clipboard xfers.
- * ref       - MyRef from Message_DataRequest, or zero.
- * **data    - Pointer to data pointer, to allow for flex blocks.
- * length    - Length of data to be transferred.
- * type      - Filetype of data.
- * *filename - Pointer to proposed leafname for data.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
-
-
-int send_start_data_save_function (wimp_w w, wimp_i i, os_coord pos, int ref, int (*save_function) (char *filename),
-                                   int length, int type, char *filename);
-
-/* Start a datasave process using a function to save the data once the pathname has been found.
- *
- * w, i, pos      - Window, icon and pointer position of destination: copy from Message_DataRequest for clipboard xfers.
- * ref            - MyRef from Message_DataRequest, or zero.
- * save_function()- Function to manage the data saving.
- * length         - Length of data to be transferred.
- * type           - Filetype of data.
- * *filename      - Pointer to proposed leafname for data.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
-
-
-int send_reply_data_save_ack (wimp_message *data_save_ack);
-
-/* Reply to a Message_DataSaveAck, following a call to send_start_data_save ().
- *
- * *data_save_ack - wimp message block.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
-
-
-int send_reply_ram_fetch (wimp_message *ram_fetch, wimp_t task_handle);
-
-/* Reply to a Message_RamFetch, following a call to send_start_data_save ().
- *
- * *ram_fetch  - wimp message block.
- * task_handle - task handle of calling task, for use by Wimp_TransferBlock.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
-
-/* ==================================================================================================================
- * Loading data
+ * \param w		The destination window handle.
+ * \param i		The destination icon handle.
+ * \param pos		The destination pointer position. \TODO -- Not a pointer ?!?
+ * \param ref		MyRef from Message_DataRequest, or zero.
+ * \param save_function	Callback function to handle the datasave process.
+ * \param length	Length of data to be transferred.
+ * \param type		Filetype of data.
+ * \param *name		Pointer to proposed leafname for data.
+ * \return		0 if the transfer is started OK; -1 for an error.
  */
 
-int receive_reply_data_save_block (wimp_message *data_save, char **data);
+int transfer_save_start_callback(wimp_w w, wimp_i i, os_coord pos, int ref, int (*save_function) (char *filename),
+		int length, int type, char *name);
 
-/* Reply to a Message_DataSave.
+
+/**
+ * Reply to a Message_DataSaveAck, received following a call to send_start_data_save().
  *
- * *data_save - wimp message block.
+ * \param *message		The DataSave_Ack message block.
+ * \return			0 if successful; -1 if an error occurred.
+ */
+
+int transfer_save_reply_datasaveack(wimp_message *message);
+
+
+/**
+ * Reply to a Message_RamFetch, received following a call to send_start_data_save().
  *
- * ------------------------------------------------------------------------------------------------------------------ */
+ * \param *message		The Message_RAMFetch data block.
+ * \param task_handle		The task handle of the owning task.
+ * \return			0 on successful conculsion; -1 for an error.
+ */
+
+int transfer_save_reply_ramfetch(wimp_message *message, wimp_t task_handle);
 
 
-int receive_reply_data_save_function (wimp_message *data_save, int (*load_function) (char *filename));
-
-/* Reply to a Message_DataSave.
+/**
+ * Reply to a Message_DataSave initiating a file load, supplying a pointer to
+ * a flex block to take whatever data is offered.
  *
- * *data_save - wimp message block.
+ * \param *message		The Message_DataSave block.
+ * \param **data_ptr		Pointer to an unused flex block handle, to
+ *				be updated with details of a block.
+ * \return			0 on success; -1 if an error occurred.
+ */
+
+int transfer_load_reply_datasave_block(wimp_message *message, char **data_ptr);
+
+
+/**
+ * Reply to a Message_DataSave initiating a file load, supplying a callback
+ * function to be supplied with the filename.
  *
- * ------------------------------------------------------------------------------------------------------------------ */
+ * \param *message		The Message_DataSave block.
+ * \param *load_function	Callback function to be called with the filename.
+ * \return			0 on success; -1 if an error occurred.
+ */
+
+int transfer_load_reply_datasave_callback(wimp_message *message, int (*load_function) (char *filename));
 
 
-int recieve_reply_ram_transmit (wimp_message *ram_transmit, char *name);
-
-/* Reply to a Message_RAMTransmit.
+/**
+ * Reply to a Message_RAMTransmit, received following a call to
+ * transfer_load_reply_datasave_block() or transfer_load_reply_datasave_callback().
  *
- * *ram_transmit - the wimp message block.
- * *name         - pointer to string to take leafname, or NULL.
- *
- * returns int result  (0  = transfer in progress;
- *                      -1 = error occurred;
- *                      >0 = size of recieved data)
- *
- * ------------------------------------------------------------------------------------------------------------------ */
+ * \param *message		The Message_RAMFetch data block.
+ * \param name			Pointer to a buffer to take the leafname of the
+ *				file -- no buffer length checks are made.
+ * \return			0 if transfer is still in progress; >0 indicates
+ *				completion and size of data; -1 indicates error.
+ */
+
+int transfer_load_reply_ramtransmit(wimp_message *message, char *name);
 
 
-int receive_bounced_ram_fetch (wimp_message *ram_fetch);
-
-/* Deal with a bounce from Message_RAMFetch.
+/**
+ * Reply to a bounced Message_RAMFetch, returned following a call to
+ * transfer_load_reply_datasave_block() or transfer_load_reply_datasave_callback().
  *
- * *ram_fetch - the wimp message block.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
+ * \param *message		The returned MessageRAMFetch message block.
+ * \return			0 if successful; -1 if an error occurred.
+ */
 
-
-void receive_init_quick_data_load_function (wimp_message *data_load, int (*load_function) (char *filename));
-
-/* Initilaise process ready to call receive_reply_data_load() if a transfer is started with a Message_DataLoad
- * (eg. from the Filer, direct).
- *
- * *data_load - the wimp message block.
- *
- * ------------------------------------------------------------------------------------------------------------------ */
+int transfer_load_bounced_ramfetch(wimp_message *message);
 
 
-int receive_reply_data_load (wimp_message *data_load, char *name);
+/**
+ * Initialise the data load process following the receipt of a direct
+ * Message_DataLoad (such as a drag direct from the Filer).
+ *
+ * \param *message		The Message_DataLoad block.
+ * \param *load_function	Callback function to be called with the filename.
+ */
 
-/* Reply to a Message_DataLoad.
+void transfer_load_start_direct_callback(wimp_message *message, int (*load_function) (char *filename));
+
+
+/**
+ * Reply to a Message_DataLoad, received following a call to
+ * transfer_load_reply_datasave_block(), transfer_load_reply_datasave_callback(),
+ * or transfer_load_start_direct_callback.
  *
- * *data_load - the wimp message block.
- * *name      - pointer to string to take leafname, or NULL.
- *
- * returns int result  (0  = transfer in progress;
- *                      -1 = error occurred;
- *                      >0 = size of recieved data)
- *
- * ------------------------------------------------------------------------------------------------------------------ */
+ * \param *message		The Message_RAMFetch data block.
+ * \param name			Pointer to a buffer to take the leafname of the
+ *				file -- no buffer length checks are made.
+ * \return			0 if transfer is still in progress; >0 indicates
+ *				completion and size of data; -1 indicates error.
+ */
+
+int transfer_load_reply_dataload(wimp_message *message, char *name);
 
 #endif
+
