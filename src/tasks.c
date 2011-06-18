1c1ac54f6c6538f9a54616db602d57a848ef46de
Steve Fryatt <info@stevefryatt.org.uk>
2011-06-18 17:50:47 +0000

    Refactor Tasks module.

--- src/tasks.c
+++ src/tasks.c
@@ -1,6 +1,11 @@
-/* SF-Lib - Tasks.c
+/**
+ * \file: tasks.c
  *
- * Version 0.10 (4 July 2009)
+ * SF-Lib - Tasks.c
+ *
+ * (C) Stephen Fryatt, 2009-2011
+ *
+ * Wimp Task detection and Task Manager support.
  */
 
 /* OS-Lib header files. */
@@ -19,61 +24,55 @@
 
 #include <string.h>
 
-/* ================================================================================================================== */
 
-osbool test_for_running_task (char *task_name, wimp_t ignore_task)
+/* Check if a named task is running.
+ *
+ * This is an external interface, documented in tasks.h
+ */
+
+osbool tasks_get_running(char *task_name, wimp_t ignore_task)
 {
-  taskmanager_task task_data;
-  int              next;
-  osbool           found;
-  char             *end;
+	taskmanager_task	task_data;
+	int			next = 0;
+	osbool			found = FALSE;
+	char			*end;
 
+	while (next >= 0) {
+		next = taskmanager_enumerate_tasks(next, &task_data, sizeof(taskmanager_task), &end);
 
-  next = 0;
-  found = FALSE;
+		if (end > (char *) &task_data && strcmp(task_data.name, task_name) == 0 && task_data.task != ignore_task)
+			found = TRUE;
+	}
 
-  while (next >= 0)
-  {
-    next = taskmanager_enumerate_tasks (next, &task_data, sizeof (taskmanager_task), &end);
-
-    if (end > (char *) &task_data)
-    {
-      if (strcmp (task_data.name, task_name) == 0 && task_data.task != ignore_task)
-      {
-        found = TRUE;
-      }
-    }
-  }
-
-  return (found);
+	return found;
 }
 
-/* ================================================================================================================== */
 
-osbool test_for_duplicate_task (char *task_name, wimp_t handle, char *message, char *buttons)
+/* Test for a duplicate copy of the named task, ignoring the task with the
+ * specified handle.  If one is found, ask the user whether to quit.
+ *
+ * This is an external interface, documented in tasks.h
+ */
+
+osbool tasks_test_for_duplicate (char *task_name, wimp_t handle, char *message, char *buttons)
 {
-  osbool                   running, exit;
-  char                     mbuf[1024], bbuf[128];
-  wimp_error_box_selection result;
+	osbool			running, exit = FALSE;
+	char			mbuf[1024], bbuf[128];
 
-  /* Test to see if a task of the given name is running. */
+	/* Test to see if a task of the given name is running. */
 
-  running = test_for_running_task (task_name, handle);
-  exit = FALSE;
+	running = tasks_get_running(task_name, handle);
 
-  /* If one is found, offer the user the chance to cancel loading the current version. */
+	/* If one is found, offer the user the chance to cancel loading the current version. */
 
-  if (running)
-  {
-    msgs_param_lookup (message, mbuf, sizeof(mbuf), task_name, NULL, NULL, NULL);
-    msgs_lookup (buttons, bbuf, sizeof(bbuf));
+	if (running) {
+		msgs_param_lookup(message, mbuf, sizeof(mbuf), task_name, NULL, NULL, NULL);
+		msgs_lookup(buttons, bbuf, sizeof(bbuf));
 
-    result = error_report_question (mbuf, bbuf);
+		if (error_report_question(mbuf, bbuf) == 1)
+			exit = TRUE;
+	}
 
-    exit = (result == 1);
-  }
-
-  return (exit);
+	return exit;
 }
 
-/* ================================================================================================================== */
--- src/tasks.h
+++ src/tasks.h
@@ -1,10 +1,15 @@
-/* SF-Lib - Tasks.h
+/**
+ * \file: tasks.h
  *
- * Version 0.10 (4 July 2009)
+ * SF-Lib - Tasks.h
+ *
+ * (C) Stephen Fryatt, 2009-2011
+ *
+ * Wimp Task detection and Task Manager support.
  */
 
-#ifndef _SFLIB_TASKS
-#define _SFLIB_TASKS
+#ifndef SFLIB_TASKS
+#define SFLIB_TASKS
 
 #include "oslib/wimp.h"
 #include "oslib/types.h"
@@ -13,15 +18,34 @@
 
 /* ================================================================================================================== */
 
-osbool test_for_running_task (char *task_name, wimp_t ignore_task);
-
-/* Returns TRUE if a task with the given name is already running.  If ignore_task is a valid task handle, then
+/**
+ * Check if a named task is running.  If ignore_task is a valid task handle, then
  * that task will be ignored in the comparison.
+ *
+ * \param *task_name		The name to test against.
+ * \param ignore_task		A task handle to ignore, even if the name matches.
+ * \return			TRUE if a match was found; else FALSE.
  */
 
-osbool test_for_duplicate_task (char *task_name, wimp_t handle, char *message, char *buttons);
+osbool tasks_get_running(char *task_name, wimp_t ignore_task);
 
-/* Test for a duplicate copy of the task, and return TRUE if the current task should quit as a result.
+
+/**
+ * Test for a duplicate copy of the named task, ignoring the task with the
+ * specified handle.  If one is found, ask the user whether to quit.
+ *
+ * \param *task_name		The name of the task to test for.
+ * \param handle		A task handle to ignore, even if the name matches.
+ * \param *message		A MessageTrans token for the question to ask if
+ *				a match is found.
+ * \param *buttons		A MessageTrans token for the button list to use
+ *				in the question.  The first button must be
+ *				the one to quit.
+ * \return			TRUE if the task should quit (ie. the first
+ *				option was selected by the user.
  */
+
+osbool tasks_test_for_duplicate(char *task_name, wimp_t handle, char *message, char *buttons);
 
 #endif
+
