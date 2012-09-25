702f05878e1c8cad6addd8e9d36332623a859a64
Steve Fryatt <info@stevefryatt.org.uk>
2012-09-25 23:06:47 +0000

    Add licence info to all source files.

--- src/resources.h
+++ src/resources.h
@@ -1,9 +1,28 @@
+/* Copyright 2003-2012, Stephen Fryatt
+ *
+ * This file is part of SFLib:
+ *
+ *   http://www.stevefryatt.org.uk/software/
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ *   http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
 /**
  * \file: resources.h
- *
- * SF-Lib - Resources.h
- *
- * (C) Stephen Fryatt, 2003-2011
  *
  * Generic RISC OS resource handling for internationalisation and other
  * shared data.
--- src/url.h
+++ src/url.h
@@ -1,9 +1,28 @@
+/* Copyright 2003-2012, Stephen Fryatt
+ *
+ * This file is part of SFLib:
+ *
+ *   http://www.stevefryatt.org.uk/software/
+ *
+ * Licensed under the EUPL, Version 1.1 only (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ *   http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
 /**
  * \file: url.h
- *
- * SF-Lib - URL Dispatch System.
- *
- * (C) Stephen Fryatt, 2003-2011
  *
  * URL dispatch code, which takes the following steps to broadcast a URL to other
  * applications on the machine:
