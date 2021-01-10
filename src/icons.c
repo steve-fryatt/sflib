c5abb217aaa2b4a3bd010cdbc672480e87d17d35
Steve Fryatt <info@stevefryatt.org.uk>
2021-01-10 16:50:50 +0000

    Fix out-by-one bounds checks on icon text copy.

--- src/icons.c
+++ src/icons.c
@@ -67,12 +67,12 @@ char *icons_copy_text(wimp_w w, wimp_i i, char *buffer, size_t length)
 	wimp_get_icon_state(&icon);
 
 	if (icon.icon.flags & wimp_ICON_INDIRECTED) {
-		string_ctrl_copy(buffer, icon.icon.data.indirected_text.text, length - 1);
+		string_ctrl_copy(buffer, icon.icon.data.indirected_text.text, length);
 	} else if (length > 12) {
 		string_ctrl_strncpy(buffer, icon.icon.data.text, 12);
 		buffer[12] = '\0';
 	} else {
-		string_ctrl_copy(buffer, icon.icon.data.text, length - 1);
+		string_ctrl_copy(buffer, icon.icon.data.text, length);
 	}
 
 	return buffer;
