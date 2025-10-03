70b7f144bb0344970352c61ff072f87f214d0b3d
Steve Fryatt <info@stevefryatt.org.uk>
2025-10-03 21:57:29 +0100

    Update Puzzle type with official allocation

--- src/dataxfer.h
+++ src/dataxfer.h
@@ -41,10 +41,10 @@
  * Filetypes which aren't defined in OSLib.
  */
 
-#define dataxfer_TYPE_PUZZLE	0x05fu
 #define dataxfer_TYPE_LOCATE	0x1a1u
 #define dataxfer_TYPE_CASHBOOK	0x1cau
 #define dataxfer_TYPE_PRINTPDF	0x1d8u
+#define dataxfer_TYPE_PUZZLE	0x1f2u
 #define dataxfer_TYPE_PDF	0xadfu
 #define dataxfer_TYPE_FANCYTEXT	0xaf8u
 #define dataxfer_TYPE_CSV	0xdfeu
